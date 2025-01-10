8bbf51a04cfb6a1e5cddbdbff2433b0b1e6917c8
Jacky Eastmoon <jacky_eastmoon@hotmail.com>
2025-01-10 17:20:14 +0800

    feat: add path resolve in vite and typescript config

--- app/web/test/calculator.spec.ts
+++ app/web/test/calculator.spec.ts
@@ -1,5 +1,5 @@
 import { assert } from "chai";
-import { addition } from "@app/model/service/calculator";
+import { addition } from "@/model/service/calculator";
 
 describe("Calculator Tests", () => {
       it("should return 5 when 2 is added to 3", () => {
--- app/web/vite.config.ts
+++ app/web/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from "path"
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -9,5 +10,10 @@ export default defineConfig({
     watch: {
       usePolling: true
     }
+  },
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "./src"),
+    },
   },
 })
