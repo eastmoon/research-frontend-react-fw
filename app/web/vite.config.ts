00b16ebeb060c7a97f0dea5b3f35cc91b2238866
Jacky Eastmoon <jacky_eastmoon@hotmail.com>
2025-01-10 18:38:01 +0800

    Revert "feat: simplify alias setting"
    This reverts commit 3c30647db5af534f688599d6a44d6b3a2a340dd4.

--- app/web/vite.config.ts
+++ app/web/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from "path"
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -12,7 +13,7 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      "@": "./src",
+      "@": resolve(__dirname, "./src"),
     },
   },
 })
